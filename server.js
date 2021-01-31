212fd3e8b187ec95ba8fd4eb754af891e74a6b23
Reza Younesi <younesi.nima@gmail.com>
2021-01-31 12:20:19 -0500

    v3

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/samplePost'));
 app.get('/*', function(req,res) {
+  console.log("reza here")
+  console.log(__dirname)
+
   res.sendFile(path.join(__dirname+
     '/dist/samplePost/index.html'));});
 app.listen(process.env.PORT || 8080);
